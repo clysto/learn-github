03693e023faabffc345f6c008258fc983d05065b
clysto <maoyachen55@gmail.com>
2020-04-10 18:13:40 +0800

    加法功能

--- index.test.js
+++ index.test.js
@@ -4,3 +4,8 @@ test('uuid', () => {
   const greeting = new Greeting();
   expect(greeting.hi()).toMatch(/你好世界.+/);
 });
+
+test('add', () => {
+  const greeting = new Greeting();
+  expect(greeting.add(20, 6)).toEqual(26);
+});
